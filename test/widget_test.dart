27e64c2fe482ba6e79a6c7aac90ab84a99a333d7
Naaarezai <nasratollah.rezai@gmail.com>
2026-01-25 22:58:15 +0200

    🧹 Lint: Remove unused import in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:naapurisapuska/main.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 
 void main() {
